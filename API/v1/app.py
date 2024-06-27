874ded334f071450838788ddcd4b642bc01968b5
BenBet21 <benoit.beti@gmail.com>
2024-06-27 15:54:10 +0200

    2. Implementing Authentication with JWT

--- API/v1/app.py
+++ API/v1/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from API.v1.auth import auth as auth_blueprint
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///development.db'
+app.config['JWT_SECRET_KEY'] = 'motdepasse'
+app.register_blueprint(auth_blueprint, url_prefix='/auth')
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
