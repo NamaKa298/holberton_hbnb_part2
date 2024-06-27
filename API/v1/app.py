59c4d5d424384d0e66d95c347bb28df83a875919
BenBet21 <benoit.beti@gmail.com>
2024-06-27 16:05:02 +0200

    2. Implementing Authentication with JWT

--- API/v1/app.py
+++ API/v1/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from API.v1.auth import auth as auth_blueprint
-from config import DevelopmentConfig, ProductionConfig, TestingConfig
+from config import DevelopmentConfig, ProductionConfig, TestingConfig, Config
 import os
 
 app = Flask(__name__)
@@ -13,7 +13,9 @@ elif os.getenv('ENV') == 'testing':
     app.config.from_object(TestingConfig) 
 else:
     app.config.from_object(ProductionConfig)
+
 app.config['JWT_SECRET_KEY'] = 'motdepasse'
+app.config.from_object(Config)
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
--- config.py
+++ config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    JWT_SECRET_KEY = 'motdepasse'
 
 class DevelopmentConfig(Config):
     DEBUG = True
