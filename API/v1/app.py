9fda2279e5131836aa13c249307004ec4113cc17
NamaKa298 <marion.saint-martin@protonmail.com>
2024-06-27 15:54:02 +0200

    task 1

--- API/v1/app.py
+++ API/v1/app.py
@@ -1,6 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import DevelopmentConfig, ProductionConfig, TestingConfig
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///development.db'
+
+if os.getenv('ENV') == 'development':
+    app.config.from_object(DevelopmentConfig)
+elif os.getenv('ENV') == 'testing':
+    app.config.from_object(TestingConfig) 
+else:
+    app.config.from_object(ProductionConfig)
+
 db = SQLAlchemy(app)
+
+@app.before_first_request
+def initialize_database():
+    """Crée toutes les tables définies dans les modèles si elles n'existent pas."""
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run()
