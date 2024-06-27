d6fa68ba00dd8b3d79fda89d6dd18c50f07ac788
BenBet21 <benoit.beti@gmail.com>
2024-06-27 16:59:28 +0200

    2. Implementing Authentication with JWT

--- API/v1/auth.py
+++ API/v1/auth.py
@@ -5,13 +5,14 @@ from werkzeug.security import check_password_hash
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token
 from werkzeug.security import check_password_hash
-from Model.user import User
 
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['POST'])
 def login():
+    from Model.user import User
+    
     email = request.json.get('email', None)
     password = request.json.get('password', None)
 
